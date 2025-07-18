46a3d9d2bb3cd97ba27a74c1c0aba49c6c96262b
rakesh <rakesh.p@anugraha.co>
2025-07-18 14:26:57 +0530

    server started

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
+
+const app = express();
+const port = 3000;
+await connectDB()
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is Live");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
