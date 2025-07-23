3b704aee4294241c7070baa2182dd4d319cdc6d8
rakesh <rakesh.p@anugraha.co>
2025-07-24 00:00:58 +0530

    added till addshows 7,44

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,6 +4,7 @@ export const protectAdmin = async (req, res, next) => {
   try {
     const { userId } = req.auth();
     const user = await clerkClient.users.getUser(userId);
+    console.log(user,user.privateMetadata.role,"xxxy")
     if (user.privateMetadata.role !== "admin") {
       return res.json({
         success: false,
@@ -12,6 +13,7 @@ export const protectAdmin = async (req, res, next) => {
     }
     next();
   } catch (error) {
+    console.log(error,"eee")
     return res.json({
       success: false,
       message: "note authorized",
